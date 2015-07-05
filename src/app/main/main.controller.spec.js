318e0ee2bea705c5a0237f032e4170fe5ffdc3a0
Giovanni Ferron <gvnnrulez@gmail.com>
2015-07-05 12:30:39 +1000

    Fix missing package for tests

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -4,16 +4,17 @@
 
     beforeEach(module('angularIe8'));
 
-    describe('test MainController', function(){
+    describe('test MainController', function() {
 
-      var scope, mainController;
+      var scope;
+      var mainController;
 
       beforeEach(inject(function($rootScope, $controller) {
-          scope = $rootScope.$new();
-          mainController = $controller('MainController', {$scope: scope});
+        scope = $rootScope.$new();
+        mainController = $controller('MainController', {$scope: scope});
       }));
 
-      it('should uppercase correctly', function(){
+      it('should uppercase correctly', function() {
         expect(scope.message).toEqual('world');
       });
 
